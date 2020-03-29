b3b876986484e38d8964fafbdf34ceedb2e1a702
Doug Durham <ddurham@dontpaniclabs.com>
2020-03-29 11:33:59 -0500

    Added the pseudocode

--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -6,7 +6,18 @@ namespace PersonalProjectLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Prompt the user to indicate which type of stat they wish to calculate
+                // If they select batting average
+                    // Prompt them to enter the number of hits, walks, total at bats
+                    // Calculate the batting average
+                    // Write the batting average to the console
+                // Else if they select slugging percentage
+                    // Prompt them to enter the number of singles, double, triples, home runs, and at bats
+                    // Calculate the slugging percentage
+                    // Write the slugging percentage to the console
+            // Prompt the user to see if they wish to calculate another stat
+                // If they select yes, return to the top of the program
+                //Else end the program
         }
     }
 }
